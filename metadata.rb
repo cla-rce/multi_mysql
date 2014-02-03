c24e8dcbe276885715d3b1593aa614549e64c780
Adam Mielke <adam@umn.edu>
2014-02-03 15:48:10 -0600

    Calling this version 1.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'adam@umn.edu'
 license          'Apache 2.0'
 description      'Installs/Configures multiple MySQL server instances'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.0'
 
 depends          'openssl', '~> 1.1'
 depends          'ark', '~> 0.4'
